1084fbedc33fc4f6b28e8fa4a2a5661ead32f3d3
SuzianeS <suzianewgr@gmail.com>
2024-04-11 10:02:20 -0300

    feat: adicionando implementação calculo de datas

--- src/BrasilDevKit.Ferramentas/Matematica/Datas/CalculadoraDeData.cs
+++ src/BrasilDevKit.Ferramentas/Matematica/Datas/CalculadoraDeData.cs
@@ -1,7 +1,13 @@
-namespace BrasilDevKit.Ferramentas.Matematica.Datas
-{
-    public class CalculadoraDeData
-    {
+namespace BrasilDevKit.Ferramentas.Matematica.Datas;
 
-    }
+public static class CalculadoraDeData
+{
+    public static int CalcularDias(DateTime dataInicial, DateTime dataFinal)
+        => (dataFinal - dataInicial).Days;
+
+    public static DateTime SomarDias(DateTime dataInicial, int diasASeremAdicionados)
+        => dataInicial.AddDays(diasASeremAdicionados);
+
+    public static DateTime SubtrairDias(DateTime dataInicial, int diasASeremSubtraidos)
+        => dataInicial.AddDays(-diasASeremSubtraidos);
 }
