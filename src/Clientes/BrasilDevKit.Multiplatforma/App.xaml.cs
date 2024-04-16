36b9dd86810321b1e56c924c42085849f60eac36
Alex Mariano <alexmarianokotcz@outlook.com.br>
2024-04-15 22:47:01 -0300

    feat: criando projeto multi-plataforma (MAUI) #10

---
+++ src/Clientes/BrasilDevKit.Multiplatforma/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BrasilDevKit.Multiplatforma;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ src/Clientes/BrasilDevKit.Multiplatforma/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BrasilDevKit.Multiplatforma;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
