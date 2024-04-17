325ca0286f1511f9ee7bb93f35e2271083f771b0
lucasmarianokotcz <lucasmariano-@hotmail.com>
2024-04-17 00:30:06 -0300

    fix: modificação da classe CalculadoraDeDatas com campos DateOnly. fix: correção no método CalcularDias da classe CalculadoraDeDatas.
    #6 e #7

--- src/BrasilDevKit.Ferramentas/Matematica/Datas/CalculadoraDeData.cs
+++ src/BrasilDevKit.Ferramentas/Matematica/Datas/CalculadoraDeData.cs
@@ -2,12 +2,12 @@ namespace BrasilDevKit.Ferramentas.Matematica.Datas;
 
 public static class CalculadoraDeData
 {
-    public static int CalcularDias(DateTime dataInicial, DateTime dataFinal)
-        => (dataFinal - dataInicial).Days;
+    public static int CalcularDias(DateOnly dataInicial, DateOnly dataFinal)
+        => Math.Abs(dataInicial.DayNumber - dataFinal.DayNumber);
 
-    public static DateTime SomarDias(DateTime dataInicial, int diasASeremAdicionados)
+    public static DateOnly SomarDias(DateOnly dataInicial, int diasASeremAdicionados)
         => dataInicial.AddDays(diasASeremAdicionados);
 
-    public static DateTime SubtrairDias(DateTime dataInicial, int diasASeremSubtraidos)
+    public static DateOnly SubtrairDias(DateOnly dataInicial, int diasASeremSubtraidos)
         => dataInicial.AddDays(-diasASeremSubtraidos);
 }
