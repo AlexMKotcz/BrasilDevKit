4057b4e00a0d55099fffea113a7dc9bc966b7c7b
Alex <alexmarianokotcz@outlook.com.br>
2024-05-08 23:54:18 -0300

    style: resolvendo avisos do compilador para melhorar a estilização do código

--- src/BrasilDevKit.Ferramentas/Computacao/Validadores/IValidadorSimples.cs
+++ src/BrasilDevKit.Ferramentas/Computacao/Validadores/IValidadorSimples.cs
@@ -1,15 +1,14 @@
-namespace BrasilDevKit.Ferramentas.Computacao.Validadores
+namespace BrasilDevKit.Ferramentas.Computacao.Validadores;
+
+/// <summary>
+/// Define um contrato para validadores simples.
+/// </summary>
+public interface IValidadorSimples
 {
     /// <summary>
-    /// Define um contrato para validadores simples.
+    /// Valida um valor.
     /// </summary>
-    public interface IValidadorSimples
-    {
-        /// <summary>
-        /// Valida um valor.
-        /// </summary>
-        /// <param name="valor">O valor a ser validado.</param>
-        /// <returns>True se o valor for válido, False caso contrário.</returns>
-        bool Validar(string valor);
-    }
+    /// <param name="valor">O valor a ser validado.</param>
+    /// <returns>True se o valor for válido, False caso contrário.</returns>
+    bool Validar(string valor);
 }
