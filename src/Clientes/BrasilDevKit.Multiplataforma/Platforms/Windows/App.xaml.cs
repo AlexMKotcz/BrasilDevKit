f85348b58b8f5c5331baae4a7bfb07841f38401a
Alex <alexmarianokotcz@outlook.com.br>
2024-05-12 22:54:27 -0300

    fix: correção para build do projeto

--- src/Clientes/BrasilDevKit.Multiplataforma/Platforms/Windows/App.xaml.cs
+++ src/Clientes/BrasilDevKit.Multiplataforma/Platforms/Windows/App.xaml.cs
@@ -1,5 +1,4 @@
-﻿using BrasilDevKit.Multiplatforma.WinUI;
-// To learn more about WinUI, the WinUI project structure,
+﻿// To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace BrasilDevKit.Multiplatforma.Platforms.Windows;
