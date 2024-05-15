9d7e57e15990af2c5b44bdf13ebc25ee3973c0c8
Alex <alexmarianokotcz@outlook.com.br>
2024-05-14 23:05:52 -0300

    feat: adicionando descrição ao enum tipoCalculo, adicionando método de extensão

--- src/Clientes/Clientes.Auxiliares/TipoCalculo.cs
+++ src/Clientes/Clientes.Auxiliares/TipoCalculo.cs
@@ -1,8 +1,13 @@
-﻿namespace Clientes.Auxiliares;
+﻿using System.ComponentModel;
+
+namespace Clientes.Auxiliares;
 
 public enum TipoCalculo
 {
+    [Description("Diferença entre datas")]
     DiferencaDatas,
+    [Description("Adicionar dias")]
     AdicionarDias,
+    [Description("Subtrair dias")]
     SubtrairDias
 }
